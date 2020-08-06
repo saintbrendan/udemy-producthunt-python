db7d8679ee4321c4c9008aa185356f279c4306a2
Brendan Cunnie <saintbrendan@gmail.com>
2020-08-05 23:39:52 -0400

    Update Signup, Login, and views.py to return on call to signup(request)

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        # Create account
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords must match'})
 
+    else:
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
-
 
 def logout(request):
     # TODO Nee to logout and route to home page
